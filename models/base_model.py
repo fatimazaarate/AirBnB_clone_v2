259966dd261d901de73b51a5f58ccedfd04b5d06
hafsachenni <hafsachenni77@gmail.com>
2024-01-14 06:58:04 -0800

    db storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,19 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
