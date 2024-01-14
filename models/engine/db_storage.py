11820b3a8afd3ea7e2427cf54c93816de13c4f7d
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-14 18:10:36 +0100

    all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,11 +4,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from os import getenv
 from models.base_model import BaseModel, Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
     __engine = None
     __session = None
+    
 
     def __init__(self):
         self.engine = create_engine(
@@ -29,3 +36,21 @@ class DBStorage:
 
         Session = sessionmaker(bind=self.__engine)
         self.__session = Session()
+
+    def all(self, cls=None):
+        """uery on the current database session (self.__session)
+        all objects depending of the class name"""
+        if cls is None:
+            classes = [State, City, User, Place, Review, Amenity]
+            objects = []
+            for classe in classes:
+                objects.extend(self.__session.query(classe).all())
+        else:
+            if isinstance(cls, str):
+                cls = eval(cls)
+            objects = self.__session.query(cls).all()
+        
+        result = {}
+        for obj in objects:
+            result["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        return result
