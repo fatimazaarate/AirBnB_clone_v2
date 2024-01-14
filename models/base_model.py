ea132b16b977e6b32ab4989547bcb6c013da32d0
hafsachenni <hafsachenni77@gmail.com>
2024-01-14 04:17:04 -0800

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -38,3 +38,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def __repr__(self):
+        """ returns str repre of basemodel"""
+        return (self.__str__())
