00664dba4f83299ae5447ea5aa2ac09e4670a70c
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-31 19:47:30 +0100

    task

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static
+
+# Update package list
+sudo apt-get update
+
+# Install Nginx if it not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get install -y nginx
+fi
+
+# Create directories for web_static deployment
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file for testing
+echo "<html>
+<head></head>
+<body>Holberton School</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link to test folder
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+echo -e "location /hbnb_static { \n\talias /data/web_static/current/; \n }" >> /etc/nginx/sites-available/default
+
+# Restart Nginx to apply changes
+sudo service nginx restart
