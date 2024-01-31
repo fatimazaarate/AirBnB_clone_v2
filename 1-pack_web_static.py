95e808bc387a1ae0eb627524a1c3ba6652e6205b
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-01 00:24:36 +0100

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,17 +5,15 @@ using the function do_pack.
 """
 from datetime import datetime
 from fabric.api import local
-from fabric.api import local
 
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
 
     try:
-        if isdir("versions") is False:
-            local("mkdir versions")
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_name = "versions/web_static_{}.tgz".format(date)
+        local("mkdir -p versions")
+        date_format = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(date_format)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
