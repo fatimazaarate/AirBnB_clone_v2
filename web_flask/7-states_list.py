02b3a108df5fbe7104937d55d707fb464ab21dee
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-16 18:19:46 +0100

    task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """display a HTML page: (inside the tag BODY)"""
+    states = storage.all(state)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
