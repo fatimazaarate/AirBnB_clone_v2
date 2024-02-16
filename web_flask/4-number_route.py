5926963da91082887a6bf5c4ca3887ee0dd1af52
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-16 10:33:15 +0100

    task

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """
+    Display Hello HBNB.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+    Display HBNB.
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+    """
+    Display “C ” followed by the value of the text variable.
+    Args:
+        text (str): The text provided in the URL parameter.
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_py(text):
+    """
+    Display “Python ”, followed by the value of the text variable.
+    Args:
+        text (str): The text provided in the URL parameter.
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def display_n(n):
+    """
+    Display “n is a number” only if n is an integer.
+    """
+    if isinstance(n, int):
+        return "n is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
