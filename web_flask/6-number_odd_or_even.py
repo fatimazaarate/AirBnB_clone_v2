21299bc999a9acbaae794037835d8455dc0d4d53
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-16 11:31:37 +0100

    task

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,5 +58,13 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Display “n is a HTML page only if n is an integer.
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
